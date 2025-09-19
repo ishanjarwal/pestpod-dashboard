120ae2afdd2059f11772f0c888d9c8acd436089c
ishanjarwal <ishan.professional01@gmail.com>
2025-09-19 19:56:37 +0530

    vite setup with ts + tailwind + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-xl text-center w-full flex justify-center items-center h-screen">
+      <Button className="cursor-pointer">Homepage</Button>
+    </div>
+  );
+};
+
+export default App;
